85762a312430cfa46a5854d02c4c7f6c12211670
Greg Maurer <gregmaurer@gmail.com>
2016-11-14 00:15:01 -0700

    New spei tools

---
+++ spei.r
@@ -0,0 +1,61 @@
+
+
+library('SPEI')
+library('xts')
+
+get_cwdiff <- function(precip, pet){
+    # Get climatic water difference from precip and pet data
+    # precip: (xts object) weekly or monthly precipitation sums
+    # pet   : (xts object) weekly or monthly potential ET sums
+    cwdiff <- precip - pet
+    return(cwdiff)
+}
+
+get_spei <- function(cwdiff, freq=12, int_per=6, plot=TRUE){
+    # Get SPEI values from climatic water differential at given frequency and
+    # integration period. Optionally make a plot of the data
+    # ---In---
+    # cwdiff    : (xts obj) climatic water differential time series
+    # freq      : (int) frequency of cwdiff estimates (12=monthly, 52=weekly)
+    # int_per   : (int) number of periods (weeks, months) used to calculate SPEI
+    # plot      : (bool) Flag for making diagnostic plot of SPEI
+    # ---Out---
+    # spei_int  : (xts obj) spei time series at given integration period
+    # spei_int_interp: (xts obj) spei_int with NA values interpolated over
+
+
+    # Get start date
+    startmon <- as.yearmon(index(cwdiff[1]))
+    startyr <- floor(as.numeric(startmon))
+    startmon <- as.numeric( format( startmon, '%m'))
+
+    # Get spei for that integration period
+    spei_int <- spei(ts(cwdiff, frequency=freq, start=c(startyr, startmon)),
+                     int_per, na.rm=TRUE)
+
+    # Check for invalid values
+    values <- spei_int$fitted
+    values[!is.finite(values)] <- NA
+    if (sum(is.na(values)) > (int_per - 1)){
+        print('WARNING!!! - there are invalid values in the SPEI series')
+    }
+
+
+    if (plot){
+        plot(spei_int)
+    }
+    # Extract the spei values from the returned object and make xts
+    #spei_xts <- xts(as.vector(spei_int$fitted),  order.by=index(cwdiff))
+    
+    
+    # There may be both infinite and NA values in the output
+    # Convert -Inf to NA
+    #print(sum(is.na(spei_xts)))
+    #spei_xts[!is.finite(spei_xts)] <- NA
+    #print(sum(is.na(spei_xts)))
+    
+    # Interpolate over NA values
+    #spei_xts_interp <- na.approx(spei_xts)
+
+    return(spei_int)
+}
