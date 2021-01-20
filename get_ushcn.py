c5a5c236d4740fb5ebfacc68e7b41f6f69d407fe
Gregory Maurer <gregmaurer@gmail.com>
2021-01-19 20:55:57 -0700

    Get monthly function done
    * Works for both the latest and 2014 datasets
    * Data folder re-organized

--- get_ushcn.py
+++ get_ushcn.py
@@ -1,5 +1,7 @@
 import os
+import numpy as np
 import pandas as pd
+#import pdb
 
 """
 Code for getting USHCN version 2.5 data and returning as pandas dataframes.
@@ -12,34 +14,36 @@ transitioning to storage at ESS Dive). Those are labeled 2014.
 """
 
 default_basepath = '/home/greg/data/rawdata/NCDC/ushcn_v2.5/'
+valid_vars = ['prcp', 'tavg', 'tmax', 'tmin']
 
-def get_filename(dataset, varname, stationid = 'all',
+def get_filename(dataset, varname, stationids='all',
         basepath = default_basepath):
     """
     Function to get the correct filename for a variable/version/path
     Possible versions = 'latest' and '2014'
     Possible varnames = 'prcp', 'tavg', 'tmax', and 'tmin'
     """
-    if not isinstance(stationid, list):
-        stationid = [stationid]
+    # Create a list if needed and check for invalid variable names
+    if not isinstance(stationids, list):
+        stationids = [stationids]
+    if varname not in valid_vars:
+        raise ValueError('Not a valid variable type (select prcp, tavg, tmax, '
+                + 'or tmin.)')
+    # Fetch filenames from the dataset
     if dataset == 'latest':
         fdir = os.path.join(basepath,'ushcn.v2.5.5.20210118')
         files = os.listdir(fdir)
         varfiles = [f for f in files if varname in f]
-        if stationid[0] is not 'all':
-            varfiles = [f for f in varfiles if any(s in f for s in stationid)]
-
-        if stationid is None:
-            raise ValueError('Missing parameter - Provide a valid USHCN ' +
-                    'station ID.')
-        #fname = stationid + '.FLs.52j.' + varname
+        if 'all' not in stationids:
+            varfiles = [f for f in varfiles if any(s in f for s in stationids)]
         fpath = [os.path.join(fdir, f) for f in varfiles]
 
     elif dataset == '2014':
-        fname = 'ushcn2014_FLs_52j_' + varname + '.txt'
-        fpath = os.path.join(basepath, pathname, fname)
+        fname = 'ushcn2014_FLs_52i_' + varname + '.txt'
+        fpath = os.path.join(basepath, fname, fname)
+    else:
+        raise ValueError('Only "latest" and "2014" are accepted datasets.')
     
-    #print(fpath)
     return(fpath)
 
 
@@ -54,113 +58,139 @@ def get_stationsfile(basepath = default_basepath):
                 'comp3','utcoffset'])
     return(staid)
 
-def station_subset(ushcn_df, staid):
+def station_subset(df, stationids):
     """
     Drop all rows except for given staion
     """
-    out = ushcn_df.loc[staid, :]
+    # Add to list if needed
+    if not isinstance(stationids, list):
+        stationids = [stationids]
+    # Copy dataframe
+    out = df.copy()
+    if 'all' not in stationids:
+        out = df.loc[df.stationid.isin(stationids), :]
     return(out)
 
 
-def dropflags(ushcn_df):
+def dropflags(df):
     """
     Drop the flag columns
     """
-    out = ushcn_df.loc[:, ['1','2','3','4','5','6','7','8',
-        '9','10','11','12','ann']]
+    out = df.loc[:,[c for c in df.columns if 'flag' not in c]]
     return(out)
 
 def load_2014(fname):
-    widths = [11,5,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3]
+    """
+    Load file from 2014 dataset
+    """
+    cwidths = [11, 5] + ([6, 1, 1, 1] * 12)
+    cnames = ['stationid','year',
+            'jan', 'dmflag1', 'qcflag1', 'dsflag1',
+            'feb', 'dmflag2', 'qcflag2', 'dsflag2',
+            'mar', 'dmflag3', 'qcflag3', 'dsflag3',
+            'apr', 'dmflag4', 'qcflag4', 'dsflag4',
+            'may', 'dmflag5', 'qcflag5', 'dsflag5',
+            'jun', 'dmflag6', 'qcflag6', 'dsflag6',
+            'jul', 'dmflag7', 'qcflag7', 'dsflag7',
+            'aug', 'dmflag8', 'qcflag8', 'dsflag8',
+            'sep', 'dmflag9', 'qcflag9', 'dsflag9',
+            'oct', 'dmflag10', 'qcflag10', 'dsflag10',
+            'nov', 'dmflag11', 'qcflag11', 'dsflag11',
+            'dec', 'dmflag12', 'qcflag12', 'dsflag12',
+            'ann', 'dmflagann', 'qcflagann', 'dsflagann']
+    print('Opening ' + fname)
     ushcn2014 = pd.read_fwf(fname, na_values=['-9999'], header=None,
-            widths=widths,
-            names=['1','janflag','2','febflag','3','marflag','4',
-                'aprflag','5','mayflag','6','junflag','7','julflag',
-                '8','augflag','9','sepflag','10','octflag','11',
-                'novflag','12','decflag','ann','annflag'])
+            widths=cwidths, names=cnames)
+
     return(ushcn2014)
 
 def load_latest(fnames):
-    widths = [11,5,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3]
+    cwidths = [11, 1, 4] + ([6, 1, 1, 1] * 12)
+    cnames = ['stationid', 'discard','year',
+            'jan', 'dmflag1', 'qcflag1', 'dsflag1',
+            'feb', 'dmflag2', 'qcflag2', 'dsflag2',
+            'mar', 'dmflag3', 'qcflag3', 'dsflag3',
+            'apr', 'dmflag4', 'qcflag4', 'dsflag4',
+            'may', 'dmflag5', 'qcflag5', 'dsflag5',
+            'jun', 'dmflag6', 'qcflag6', 'dsflag6',
+            'jul', 'dmflag7', 'qcflag7', 'dsflag7',
+            'aug', 'dmflag8', 'qcflag8', 'dsflag8',
+            'sep', 'dmflag9', 'qcflag9', 'dsflag9',
+            'oct', 'dmflag10', 'qcflag10', 'dsflag10',
+            'nov', 'dmflag11', 'qcflag11', 'dsflag11',
+            'dec', 'dmflag12', 'qcflag12', 'dsflag12']
     for i, fname in enumerate(fnames):
-        latest = pd.read_fwf(fname, na_values=['-9999'], header=None,
-                widths=widths,
-                names=['1','janflag','2','febflag','3','marflag','4',
-                    'aprflag','5','mayflag','6','junflag','7','julflag',
-                    '8','augflag','9','sepflag','10','octflag','11',
-                    'novflag','12','decflag','ann','annflag'])
-        #print(i)
+        print('Opening ' + fname)
+        latest = pd.read_fwf(fname, na_values=["", "NA","-9999"], header=None,
+                widths=cwidths, names=cnames)
         if i==0:
             latest_out = latest
         else:
             latest_out = pd.concat([latest_out, latest])
-            
+    # Drop the discard column
+    latest_out = latest_out.drop(['discard'], axis=1)
     return(latest_out)
 
 
-def get_var(varname, staid='all', dataset='latest', basepath=default_basepath,
-        drop_flags=True, to_mm=True):
+def get_monthly_var(varname, stationids='all', dataset='latest',
+        basepath=default_basepath, prep=True):
     """
     Get USHCN variable
     """
-    fname = get_filename(dataset, varname, staid)
-    #print(fname)
+    fnames = get_filename(dataset, varname, stationids) 
     if dataset=='latest':
-        df = load_latest(fname)
-    elif dataset=='2014':
-        df = load_2014(fname)
+        df = load_latest(fnames)
 
-    #if staid is not 'all':
-    #    df = station_subset(df, staid)
-    #if drop_flags:
-    #    df = dropflags(df)
-    #if to_mm:
-    #    df = (df/100)*25.4
+    elif dataset=='2014':
+        df = load_2014(fnames)
+        df = station_subset(df, stationids)
+        # Get rid of annual columns
+        df = df.iloc[:,:-4]
+
+    # Drop flags and convert to long format
+    if prep:
+        df = dropflags(df)
+        df = reshape_ts(df, varname)
+        # Convert units
+        if dataset=='2014':
+            if varname in valid_vars[1:4]:
+                # Convert F temps to degrees C
+                df.value = ((df.value/10)-32)*(5/9)
+            elif varname in valid_vars[0:1]:
+                # Convert in precip to mm
+                df.value = (df.value/100)*25.4
+        elif dataset=='latest':
+            if varname in valid_vars[1:4]:
+                # Convert temps to degrees C (from hundredths)
+                df.value = df.value/100
+            elif varname in valid_vars[0:1]:
+                # Convert precip to mm (from tenths)
+                df.value = df.value/10
 
     return(df)
 
-def get_tavg(basepath=default_basepath, staid=None, drop_flags=True,
-        to_cels=True):
-    """
-    Get USHCN average temp
-    """
-    fname = os.path.join(basepath,'ushcn2014_FLs_52i_tavg.txt',
-            'ushcn2014_FLs_52i_tavg.txt')
-    widths = [11,5,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3]
-    ushcn_tavg = pd.read_fwf(fname, na_values=['-9999'], header=None,
-            widths=widths,
-            names=['1','janflag','2','febflag','3','marflag','4',
-                'aprflag','5','mayflag','6','junflag','7','julflag',
-                '8','augflag','9','sepflag','10','octflag','11',
-                'novflag','12','decflag','ann','annflag'])
-    if staid is not None:
-        ushcn_tavg = station_subset(ushcn_tavg, staid)
-    if dropflags:
-        ushcn_tavg = dropflags(ushcn_tavg)
-    if to_cels:
-        ushcn_tavg = ((ushcn_tavg/10)-32)*(5/9)
 
-    return(ushcn_tavg)
-
-def reshape_ts(ushcn_ts, staid, varname):
+def reshape_ts(df, varname='variable'):
     """
     Reshape a standard ushcn table with a year index and month columns. Returns
     a long-form timeseries table
     """
-    ushcn_ts['year'] = ushcn_ts.index
-    #slp2.melt(id_vars=['year'],value_vars=slp2.columns, ignore_index=False)
-    df = ushcn_ts.melt(id_vars=['year'], value_vars=ushcn_ts.columns[:-2],
+    # Melt on staitionid and year
+    df_out = df.melt(id_vars=['stationid', 'year'],
+            value_vars=df.columns[2::],
             var_name='month')
-    df['day'] = 1
-    df['station'] = staid
-    df['variable'] = varname
-    df.index = pd.to_datetime(df.year.map(str) + df.month.map(str) + 
-            df.day.map(str), format='%Y%m%d')
-    df.day = df.index.days_in_month
-    df.index = pd.to_datetime(df.year.map(str) + df.month.map(str) +
-            df.day.map(str), format='%Y%m%d')
-    df = df.sort_index()
-    return(df.iloc[:,[4,5,0,1,3,2]])
-
-
-
+    # Add a column naming the variable
+    df_out['variable'] = varname
+    # Create a day column set to 1, then create datetime column
+    df_out['day'] = 1
+    df_out['date'] = pd.to_datetime(df_out.year.map(str) + '-' + df_out.month + 
+            '-' + df_out.day.map(str), format='%Y-%b-%d')
+    # Get last day of month and create a new datetime column
+    df_out.day = df_out.date.dt.days_in_month
+    df_out.date = pd.to_datetime(df_out.year.map(str) + '-' + df_out.month +
+            '-' + df_out.day.map(str), format='%Y-%b-%d')
+    # Sort
+    df_out = df_out.sort_values(by=['stationid', 'date'])
+    # Out with sensible column order
+    return(df_out.loc[:,['stationid', 'date', 'year', 'month', 'day',
+        'variable', 'value']])
